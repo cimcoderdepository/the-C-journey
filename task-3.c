b557757c1c70a42bc7faf833aa3f36fbd69b1bdc
cimcoderdepository <gloryland4ever@gmail.com>
2023-01-30 18:27:59 +0100

    a program to swap valus of two variables

--- task-3.c
+++ task-3.c
@@ -10,15 +10,20 @@ int main(void)
 {
 	int a = 0, b = 0, c = 0;
 
-	printf("Enter first value: ");
-	scanf("%d", &a);
+	printf("What would you like your first value to be: ");
+	scanf("%d", &a);	/* takes input from user */
 
-	printf("Enter second value: ");
-	scanf("%d", &b);
+	printf("and what would you like the second to be as well: ");
+	scanf("%d", &b);	/* takes input from user */
 
-	c = a;
+	c = a;	/* 'c' is helping us to store a copy of 'a' */
 
-	printf("a: %d, b: %d\n", a, b);
-	printf("a: %d, b: %d\n", b, c);
+	printf("Your swap value is as follow\n");
+	printf("a: %d, b: %d\n", a, b);	/* original values of 'a' & 'b' */
+					/* respectively during the swap, 'a' */
+					/* losses its original value to 'b' */
+
+	printf("a: %d, b: %d\n", b, c);	/* 'b' takes its new value from 'c' */
+					/* which held a copy of 'a' previously*/
 	return (0);
 }
